21c366b7fe9a53b1160bcc15d0cf0692ca193c8e
madmax7874 <rushabhshah2001@gmail.com>
2021-07-15 22:45:34 +0530

    frontend completed

--- travel/src/components/Features.js
+++ travel/src/components/Features.js
@@ -16,24 +16,24 @@ render(){
                 <div className="row">
                     <div className="col-lg-4 my-1" >
                         <div style={{border:"1px solid rgba(0,0,0,0.5)",borderRadius:"0.8rem",backgroundColor: "#98c1d9"}}>
-                            <span style={{fontSize:"1.8rem",fontWeight:"400",margin:"1rem 0.8rem"}}>Dont forget me!</span>
-                            <p> Ok, so it sounds a little boring, but making lists are the
+                            <div style={{fontSize:"1.8rem",fontWeight:"400",margin:"1.5rem 1.5rem 0rem 1.5rem"}}><span>Dont forget me!</span></div>
+                            <p style={{padding:"1rem",fontSize:"1.1rem"}}>Ok, so it sounds a little boring, but making lists are the
                             gateway to a a stress-free holiday. So what are you waiting for?
                             Note down your essentials!</p>
                         </div>
                     </div>
                     <div className="col-lg-4 my-1" >
                         <div style={{border:"1px solid rgba(0,0,0,0.5)",borderRadius:"0.8rem",backgroundColor: "#98c1d9"}}>
-                        <span style={{fontSize:"1.8rem",fontWeight:"400",margin:"1rem 0.8rem"}}>Add your travel details systematically!</span>
-                        <p>Track all your past trips and add/update your present ones. Save
+                            <div style={{fontSize:"1.8rem",fontWeight:"400",margin:"1.5rem 1.5rem 0rem 1.5rem"}}><span>Add your travel details systematically!</span></div>
+                            <p style={{padding:"1rem",fontSize:"1.1rem"}}>Track all your past trips and add/update your present ones. Save
                         all your travel experiences, share it with others and keep
                         travelling!</p>
-                        </div> 
+                        </div>
                     </div>
                     <div className="col-lg-4 my-1">
-                        <div style={{border:"1px solid rgba(0,0,0,0.5)",borderRadius:"0.8rem", backgroundColor: "#98c1d9"}}>
-                        <span style={{fontSize:"1.8rem",fontWeight:"400",margin:"1rem 0.8rem"}}>Track your expenses!</span>
-                        <p> Going on a business trip? Keep a track of how much you earn!
+                        <div style={{border:"1px solid rgba(0,0,0,0.5)",borderRadius:"0.8rem",backgroundColor: "#98c1d9"}}>
+                            <div style={{fontSize:"1.8rem",fontWeight:"400",margin:"1.5rem 1.5rem 0rem 1.5rem"}}><span>Track your expenses!</span></div>
+                            <p style={{padding:"1rem",fontSize:"1.1rem"}}>Going on a business trip? Keep a track of how much you earn!
                         Going on a trip for leisure? Keep a track of where and how much
                         you spend! Track your expenses here!</p>
                         </div>
