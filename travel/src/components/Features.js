042646be0abe95712dcde989002ff6269caba7ef
Vidhii19 <ruparelvd0919@gmail.com>
2021-07-15 20:07:01 +0530

    styling in feature

--- travel/src/components/Features.js
+++ travel/src/components/Features.js
@@ -16,15 +16,15 @@ render(){
                 <div className="row">
                     <div className="col-lg-4 my-1" >
                         <div style={{border:"1px solid rgba(0,0,0,0.5)",borderRadius:"0.8rem",backgroundColor: "#98c1d9"}}>
-                            <h3>Dont forget me!</h3>
+                            <span style={{fontSize:"1.8rem",fontWeight:"400",margin:"1rem 0.8rem"}}>Dont forget me!</span>
                             <p> Ok, so it sounds a little boring, but making lists are the
-                    gateway to a a stress-free holiday. So what are you waiting for?
-                    Note down your essentials!</p>
+                            gateway to a a stress-free holiday. So what are you waiting for?
+                            Note down your essentials!</p>
                         </div>
                     </div>
                     <div className="col-lg-4 my-1" >
                         <div style={{border:"1px solid rgba(0,0,0,0.5)",borderRadius:"0.8rem",backgroundColor: "#98c1d9"}}>
-                        <h3>Add your travel details systematically!</h3>
+                        <span style={{fontSize:"1.8rem",fontWeight:"400",margin:"1rem 0.8rem"}}>Add your travel details systematically!</span>
                         <p>Track all your past trips and add/update your present ones. Save
                         all your travel experiences, share it with others and keep
                         travelling!</p>
@@ -32,7 +32,7 @@ render(){
                     </div>
                     <div className="col-lg-4 my-1">
                         <div style={{border:"1px solid rgba(0,0,0,0.5)",borderRadius:"0.8rem", backgroundColor: "#98c1d9"}}>
-                        <h3>Track your expenses!</h3>
+                        <span style={{fontSize:"1.8rem",fontWeight:"400",margin:"1rem 0.8rem"}}>Track your expenses!</span>
                         <p> Going on a business trip? Keep a track of how much you earn!
                         Going on a trip for leisure? Keep a track of where and how much
                         you spend! Track your expenses here!</p>
