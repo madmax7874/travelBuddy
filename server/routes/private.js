23c69d6cd135569827bfcb0cf8459822f437fd29
Vidhii19 <ruparelvd0919@gmail.com>
2021-07-29 21:45:30 +0530

    list db part 2

--- server/routes/private.js
+++ server/routes/private.js
@@ -3,31 +3,39 @@ const router = express.Router();
 const { getPrivateRoute } = require("../controllers/private");
 const { protect } = require("../middleware/auth");
 
+const myList = [
+  {
+    text: "Tickets",
+    isDone: false,
+  },
+  {
+    text: "Bags",
+    isDone: true,
+  },
+  {
+    text: "Mobile",
+    isDone: true,
+  },
+  {
+    text: "Vidhi",
+    isDone: false,
+  },
+];
+
 router.route("/").get(protect, getPrivateRoute);
 router.route("/list").get(protect, (req, res) => {
-  const myList = [
-    {
-      text: "Tickets",
-      isDone: false,
-    },
-    {
-      text: "Bags",
-      isDone: true,
-    },
-    {
-      text: "Mobile",
-      isDone: true,
-    },
-    {
-      text: "Madmax",
-      isDone: false,
-    },
-  ];
+  
   console.log(req.user);
   res.status(200).send(myList);
 });
 router.route("/sendData").post(protect, (req, res) => {
   console.log(req.body);
+  const newData = {
+    text: req.body.value,
+    isDone: false 
+  }
+  myList.push(newData);
+  console.log(myList);
   res.status(200).send("hi");
 });
 
