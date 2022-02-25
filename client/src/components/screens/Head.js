305959914621f5b2b46a8edf7c75e60086bceae6
madmax7874 <rushabhshah2001@gmail.com>
2022-02-25 23:32:59 +0530

    'footer

--- client/src/components/screens/Head.js
+++ client/src/components/screens/Head.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Navbar, Nav, Image } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 
-import logo from "../../assets/logo.png";
+// import logo from "../../assets/logo.png";
 
 const navbar = {
   padding: "0.4rem 1rem",
