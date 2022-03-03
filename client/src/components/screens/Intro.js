d6d3651df7bfe0798bbe1294d362c2a6fa2607df
madmax7874 <rushabhshah2001@gmail.com>
2022-03-03 15:59:54 +0530

    added particles

--- client/src/components/screens/Intro.js
+++ client/src/components/screens/Intro.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import ReactTextTransition from "react-text-transition";
+import Particles from "react-tsparticles";
 
-import bg from "../../assets/newbg.jpeg";
+import bg from "../../assets/cb.png";
 
 const texts = ["MEMORABLE", "GREAT", "JOYFUL", "PLEASANT", "DELIGHTFUL"];
 
@@ -14,23 +15,118 @@ const intro = {
   justifyContent: "center",
   alignItems: "center",
   textAlign: "center",
-  minHeight: "400px"
+  minHeight: "500px"
 };
 
 function Intro() {
   const [textIndex,setTextIndex] = useState(0);
+  const particlesInit = (main) => {
+    console.log(main);
+  };
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
 
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTextIndex(textIndex + 1);
-    },3000);
-      return() => clearInterval(timer);
-    });
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setTextIndex(textIndex + 1);
+  //   },3000);
+  //   return() => clearInterval(timer);
+  // });
 
     return (
       <div style={intro}>
-        <h3 style={{ fontSize:"1.7rem", paddingTop: "3rem", color: "rgba(255,255,255,1)" }}>
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          style={{position:"fixed",top:80,left:0}}
+          // width={"auto"}
+          height={500}
+          options={{
+            fullScreen: {
+              enable: false,
+              zIndex: 0
+            },
+            // background: {
+            //   color: {
+            //     value: "#0d47a1",
+            //   },
+            // },
+            fpsLimit: 600,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "pause",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                bubble: {
+                  distance: 400,
+                  duration: 2,
+                  opacity: 0.8,
+                  size: 40,
+                },
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#fff",
+              },
+              links: {
+                color: "#fff",
+                distance: 150,
+                enable: true,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outMode: "bounce",
+                random: false,
+                speed: 4,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                random: true,
+                value: 5,
+              },
+            },
+            detectRetina: true,
+          }}
+        />
+
+        <h3 style={{ fontSize:"1.7rem", paddingTop: "3rem", color: "rgba(0,0,0,1)" }}>
           MAKE YOUR TRIP{" "}
           <ReactTextTransition
             text={texts[textIndex % texts.length]}
