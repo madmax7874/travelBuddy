dc8aa92ce2184b8f882ee8a3ee6db3d82f4e94f1
madmax7874 <rushabhshah2001@gmail.com>
2022-03-07 20:28:44 +0530

    tile format done

--- client/src/components/screens/AllTrips.js
+++ client/src/components/screens/AllTrips.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useForm, Controller  } from "react-hook-form";
-import { Container, Form, Table, InputGroup, Col, Card, Button } from "react-bootstrap";
+import { Container, Form, Row, Col, Card, Button } from "react-bootstrap";
 import { useAlert } from "react-alert";
 import { ClipLoader } from "react-spinners";
 import DateRangePicker from '@wojtekmaj/react-daterange-picker';
@@ -104,53 +104,42 @@ function AllTrips() {
     trip.travelDates[0] = trip.travelDates[0].split("T")[0];
     trip.travelDates[1] = trip.travelDates[1].split("T")[0];
     return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={image} />
+      <Card style={{ width: '18rem', margin:"1rem"}} key={index}>
+        <Card.Img variant="top" src={image}/>
         <Card.Body>
-          <Card.Title>{trip.destination}</Card.Title>
-          <Card.Text>{trip.travelDates[0]} to {trip.travelDates[1]}
-          </Card.Text>
-          <Link to={`/mytrips/${trip._id}`}>View</Link>
-          {" "}
-          <Link
-            to="#"     
-            onClick={() => {
-              deleteTrip(trip, index);
-            }}
-          >
-            Delete
-          </Link>
+          <Card.Title style={{textAlign:"center"}}>{trip.destination}</Card.Title>
+          <hr/>
+          <Row style={{textAlign:"center"}}>
+            <Col sm={6} style={{padding:"0.5rem"}}>
+              <Card.Subtitle>Start</Card.Subtitle>
+              <Card.Text>{trip.travelDates[0]}</Card.Text>
+            </Col>
+            <Col sm={6}  style={{padding:"0.5rem"}}>
+              <Card.Subtitle>End</Card.Subtitle>
+              <Card.Text>{trip.travelDates[1]}</Card.Text>
+            </Col>
+          </Row>
+          <div style={{margin:"0rem 1rem"}}>
+            <Button style={{fontWeight:"bold", color:"#A15447 ",padding: "0.375rem 0.5rem",backgroundColor:"#fff", marginRight:'0px'}}>
+            <Link to={`/mytrip/${trip._id}`}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
+                <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+              </svg>
+            </Link>
+            </Button>
+            {" "}
+            <Button
+              style={{fontWeight:"bold", color:"#A15447 ",padding: "0.375rem 0.5rem",backgroundColor:"#fff", marginRight:'0px', float:"right"}}
+              onClick={() =>deleteTrip(trip, index)}
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+              <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+            </svg>
+            </Button>
+          </div>
         </Card.Body>
       </Card>
-      
-      // <tr key={trip._id} index={index} id={trip._id}>
-      //   <td>{trip.destination}</td>
-      //   <td>{trip.travelDates[0]}</td>
-      //   <td>{trip.travelDates[1]}</td>
-      //   <td>
-      //     <Link
-      //       style={{
-      //         color: "#5FA054",
-      //         fontWeight: "600",
-      //       }}
-      //       to={`/mytrip/${trip._id}`}
-      //     >
-      //       View
-      //     </Link>{" "}
-      //     <Link
-      //       style={{
-      //         color: "#A15447",
-      //         fontWeight: "600",
-      //       }}
-      //       to="#"     
-      //       onClick={() => {
-      //         deleteTrip(trip, index);
-      //       }}
-      //     >
-      //       Delete
-      //     </Link>
-      //   </td>
-      // </tr>
     );
   });
 
@@ -163,19 +152,8 @@ function AllTrips() {
       );
     } else {
       return (
-        <div style={{  padding: "0.5rem", textTransform:'capitalize' }}>
-          <br />
-          <Table>
-            {/* <thead>
-              <tr>
-                <th>Place</th>
-                <th>Departure</th>
-                <th>Arrival</th>
-                <th>Operation</th>
-              </tr>
-            </thead> */}
-            <tbody>{TripComponents}</tbody>
-          </Table>
+        <div style={{  padding: "1rem", textTransform:'capitalize', display:"flex", justifyContent:"space-evenly",flexWrap:"wrap" }}>
+          {TripComponents}
         </div>
       );
     }
