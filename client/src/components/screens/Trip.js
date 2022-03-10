553bc9542804130a3a90718bcdbc1576942c06c4
madmax7874 <rushabhshah2001@gmail.com>
2022-03-10 14:42:48 +0530

    trip page ui rdone

--- client/src/components/screens/Trip.js
+++ client/src/components/screens/Trip.js
@@ -85,7 +85,7 @@ function MyTrip() {
     });
   };
 
-  const deleteMytrip = (perDayDetail,index) => {
+  const deleteMytrip = (perDayDetail, index) => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -96,7 +96,6 @@ function MyTrip() {
       confirmButtonText: "Yes, delete it!",
     }).then(async (result) => {
       if (result.isConfirmed) {
-        
         const config = {
           headers: {
             "Content-Type": "application/json",
@@ -124,98 +123,88 @@ function MyTrip() {
   const PerDayDetails = () => {
     if (myTripDetails.length === 0)
       return (
-        <h6 style={{ textAlign: "center", padding: "0.5rem",color:"#FFF"}}>
+        <h6 style={{ textAlign: "center", padding: "0.5rem", color: "#FFF" }}>
           No details added
         </h6>
       );
-    else return null;
+    else {
+      return (
+        <div style={{  padding: "1rem", textTransform:'capitalize', display:"flex", justifyContent:"space-evenly",flexWrap:"wrap" }}>
+          {myTripDetails.map((perDayDetail, index) => (
+            <Card style={{ width: '18rem', margin:"1rem"}} key={index}>
+              <Card.Body>
+                <Card.Title style={{textAlign:"center"}}>Day {index + 1}</Card.Title>
+                <hr/>
+                Morning Place: {perDayDetail.morningPlace}
+                <br />
+                Morning Food: {perDayDetail.morningFood}
+                <br />
+                Night Place: {perDayDetail.nightPlace}
+                <br />
+                Night Food: {perDayDetail.nightFood}
+                <hr/>
+                <div style={{margin:"0rem 1rem"}}>
+                  <Button style={{fontWeight:"bold", color:"#A15447 ",padding: "0.375rem 0.5rem",backgroundColor:"#fff", marginRight:'0px'}}>
+                  <Link to="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                    </svg>
+                  </Link>
+                  </Button>
+                  {" "}
+                  <Button
+                    style={{fontWeight:"bold", color:"#A15447 ",padding: "0.375rem 0.5rem",backgroundColor:"#fff", marginRight:'0px', float:"right"}}
+                    onClick={() => {deleteMytrip(perDayDetail, index); }}                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                  </svg>
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
+      );
+    }
   };
 
   return (
     <Fragment>
       {loading ? (
-        <div
-          className="app"
-          style={{
-            padding: "1rem",
-            background: "transparent linear-gradient(180deg,#F8CDDA 0%,#1D2B64 100%)",
-            width: "100%",
-          }}
-        >
-          <div style={{ textAlign: "center" }}>
-            <span
-              className="text-center mb-4"
-              style={{
-                color: "#0081a7",
-                fontWeight: "700",
-                fontSize: "2rem",
-              }}
-            >
-              My trip
-            </span>
-          </div>
-          <div
-            style={{
-              position: "relative",
-              padding: "1rem",
-              width: "100%",
-              display: "flex",
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "middle",
-            }}
-          >
-            <Card
-              style={{
-                height: "200px",
-                width: "500px",
-                paddingTop: "40px",
-                paddingLeft: "10px",
-                textAlign: "center",
-                borderRadius: "0.8rem",
-                fontSize: "20px",
-                boxShadow: "0 1rem 2rem rgba(0, 0, 0, 0.5)",
-                backgroundColor: "rgba(255,255,255,0.2)",
-              }}
-            >
-              <Card.Title style={{ fontSize: "25px" }}>
-                Destination : {trip.destination}
-              </Card.Title>
-              <Card.Body>
-                From : {trip.travelDates[0]}
-                <br /> To : {trip.travelDates[1]}
-              </Card.Body>
-            </Card>
-          </div>
-          <div
-            style={{
-              borderRadius: "5px",
-              paddingBottom: "1rem",
-              paddingTop: "1rem",
-            }}
-          >
-            <div
-              style={{
-                paddingTop: "0.2rem",
-              }}
-            >
-              <div style={{textAlign:"center"}}>
+        <div style={{ margin: "1rem" }}>
+          <Container>
+            <div style={{ textAlign: "center" }}>
               <span
+                className="text-center mb-4"
                 style={{
-                  color: "#023e8a",
-                  fontWeight: "500",
-                  fontSize: "1.7rem",
+                  fontWeight: "600",
+                  fontSize: "2rem",
+                  color: "#5FA054",
+                  textTransform: "capitalize",
                 }}
               >
-                Per Day Details
+                {trip.destination}
               </span>
+            </div>
+            <Row style={{textAlign:"center",fontSize:"1.3rem",fontWeight:"500",color:"#141850"}}>
+              <Col md={6}>
+                  Departure: {trip.travelDates[0]}
+              </Col>
+              <Col md={6}>
+                Arrival: {trip.travelDates[1]}
+              </Col>
+            </Row>
+            <div style={{paddingTop:"2rem"}}>
+              <div style={{ textAlign: "center", padding: "1rem 0rem 0rem 0rem" }}>
+                <h3 style={{ color: "#5FA054" }}>Per Day Details</h3>
               </div>
-              <Form className="input-form" onSubmit={handleSubmit} style={{marginTop:"0rem"}}>
+              <Form onSubmit={handleSubmit}>
                 <div className="row" style={{ borderRadius: "0.3rem" }}>
                   <div className="col-lg-6">
                     <Form.Group>
                       <Form.Label
-                        style={{ fontWeight: "600",marginTop:"1rem"}}
+                        style={{ fontWeight: "600", marginTop: "1rem" }}
                       >
                         Morning Place to visit
                       </Form.Label>
@@ -233,7 +222,7 @@ function MyTrip() {
                   <div className="col-lg-6">
                     <Form.Group>
                       <Form.Label
-                        style={{ fontWeight: "600",marginTop:"1rem"}}
+                        style={{ fontWeight: "600", marginTop: "1rem" }}
                       >
                         Lunch at
                       </Form.Label>
@@ -250,7 +239,7 @@ function MyTrip() {
                   <div className="col-lg-6">
                     <Form.Group>
                       <Form.Label
-                        style={{ fontWeight: "600",marginTop:"1rem"}}
+                        style={{ fontWeight: "600", marginTop: "1rem" }}
                       >
                         Night place to visit
                       </Form.Label>
@@ -267,7 +256,7 @@ function MyTrip() {
                   <div className="col-lg-6">
                     <Form.Group>
                       <Form.Label
-                        style={{ fontWeight: "600",marginTop:"1rem"}}
+                        style={{ fontWeight: "600", marginTop: "1rem" }}
                       >
                         Dinner at
                       </Form.Label>
@@ -282,84 +271,19 @@ function MyTrip() {
                     </Form.Group>
                   </div>
                 </div>
-                <br />
-                <div style={{ textAlign: "center" }}>
-                  <Button
-                    variant="primary"
+                <div style={{ textAlign: "center", margin:"1rem" }}>
+                  <button
+                    className="slide"
                     type="submit"
-                    style={{
-                      border: "1px solid #3d5a80",
-                      backgroundColor: "#98c1d9",
-                      color: "#000",
-                      borderRadius: "2rem",
-                      fontSize: "18px",
-                      fontWeight: "600",
-                      width: "150px",
-                    }}
+                    style={{fontSize:"500"}}
                   >
                     Add
-                  </Button>
+                  </button>
                 </div>
               </Form>
+              <PerDayDetails />
             </div>
-            <PerDayDetails />
-            <div>
-              <Container>
-                <Row className="justify-content-evenly">
-                  {myTripDetails.map((perDayDetail, index) => (
-                    <Col
-                      key={index}
-                      sm="6"
-                      lg="4"
-                      style={{ marginBottom: "1rem" }}
-                    >
-                      <Card
-                        style={{
-                          paddingTop: "0.5rem 0 0 0.5rem",
-                          color: "#000",
-                          backgroundColor:"#F8CDDA",
-                        }}
-                      >
-                        <Card.Title style={{ textAlign: "center" }}>
-                          Day {index + 1}
-                        </Card.Title>
-                        <Card.Body>
-                          Morning Place: {perDayDetail.morningPlace}
-                          <br />
-                          Morning Food: {perDayDetail.morningFood}
-                          <br />
-                          Night Place: {perDayDetail.nightPlace}
-                          <br />
-                          Night Food: {perDayDetail.nightFood}
-                          <br />
-                          <div style={{ textAlign: "center" }}>
-                            <Link
-                              style={{ color: "#47126b", fontWeight: "600" }}
-                              to="#"
-                              onClick={() => {
-                                deleteMytrip(perDayDetail,index);
-                              }}
-                            >
-                              Delete
-                            </Link>
-                          </div>
-                          <div
-                            className="perDayDetail"
-                            style={{
-                              alignItems: "center",
-                              display: "flex",
-                              fontSize: "18px",
-                              justifyContent: "space-between",
-                            }}
-                          ></div>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  ))}
-                </Row>
-              </Container>
-            </div>
-          </div>
+          </Container>
         </div>
       ) : (
         <div style={{ textAlign: "center", paddingTop: "200px" }}>
